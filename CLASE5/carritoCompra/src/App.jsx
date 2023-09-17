7896c5b6dba2938808d38881fb411e221654bbe7
Ramiro Kuspita <rastictac>
2023-09-17 00:16:16 -0300

    actualizando

--- CLASE5/carritoCompra/src/App.jsx
+++ CLASE5/carritoCompra/src/App.jsx
@@ -3,32 +3,32 @@ import axios from 'axios';
 
 function App() {
   const [productos, setProductos] = useState([]);
+  const [agregarShop, setagregarShop] = useState([]);
   const [carrito, setCarrito] = useState(false);
+  const [total, setTotal] = useState(0);
   useEffect(() => {
       try {
 
-        fetch('JSON/carrito.jason')
+        fetch('JSON/carrito.json')
         .then(response=>response.json())
         .then(data=>setProductos(data.productos))
 
-        
-     
       } catch (error) {
         console.error('Error fetching data:', error)
       }
 
   }, []); // [paginaActual] renderiza si cambio
 
-  function pagSiguiente() {
-    if (siguientePagina) {
-      setpaginaActual(paginaActual + 1);
-    }
+  function agregarAlCarrito(producto){
+    setagregarShop([...agregarShop,producto])
+    setCarrito(true)
+    console.log(agregarShop);
+    setTotal(total+producto.precio)
   }
 
-  function pagAnterior() {
-    if (paginaActual > 1) {
-      setpaginaActual(paginaActual - 1);
-    }
+  function eliminarProducto(producto1) {
+    console.log("Producto a eliminar:", producto1);
+    //setagregarShop(agregarShop.filter(p => p.id !== producto1.id));
   }
 
   return (
@@ -45,10 +45,10 @@ function App() {
                 </div>
                 <div className='card-body'>
                   <p>
-                    <strong>Precio: </strong> ${producto.valor}
+                    <strong>Precio: </strong> ${producto.precio}
                   </p>
                   <p>
-                    <button>Agregar al carrito</button>
+                    <button onClick={()=>agregarAlCarrito(producto)} >Agregar al carrito</button>
                   </p>
 
                 </div>
@@ -61,10 +61,35 @@ function App() {
 
       </div>
       
-      {carrito && (
+      {agregarShop.length>0 && (
+        <div>
+          <br></br>
+          <h1>Carrito de Compras</h1>
+          <div className='row'>
+            <div className='productos col-md-8'>
+              <div className='row'>
+                {agregarShop.map((product1)=>(
+                <div className='col-md-4 mt-3' key={product1.id}>
+                  <div className='card'>
+                    <div className='card-header'>
+                      <h4>{product1.nombre}</h4>
+                    </div>
+                    <div className='card-body'>
+                      <p>
+                        <strong>Precio: </strong> ${product1.precio}
+                      </p>
+                      <button onClick={()=>eliminarProducto(producto1)} >Eliminar</button>
+                      
+                    </div>
+                  </div>
+                </div>
+                ))}
+              </div>
+            </div>
+          </div>
 
-        HOLA
-
+          <p>Total: ${total}</p>
+        </div>
       )}
 
 
